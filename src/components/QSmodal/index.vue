e3a48c88509590f8c0b8bf303a0438ef06e00e72
hzCat <q302144074@hotmail.com>
2018-01-18 00:37:21 +0800

    部分逻辑测试

--- src/components/QSmodal/index.vue
+++ src/components/QSmodal/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="modal-con">
+  <div class="modal-con" v-if="open">
       <div class="modal">
-          <div class="header">第 {{Qindex}} 题</div>
+          <div class="header">第 {{index+1}} 题</div>
           <div class="name">{{Qname||'提问人'}} :</div>
-          <div class="question">{{question}}</div>
+          <div class="question">{{question.question}}</div>
           <div class="answer">
-            <div class="select" v-for="(item,index) in selects" :key="index" @click="choose(index)">
+            <div class="select" v-for="(item,index) in question.selects" :key="index" @click="choose(index)">
               <span>{{index+1}} .</span>
               <span>{{item.content}}</span>
             </div>
@@ -17,17 +17,17 @@
 <script>
 import "./style.css";
 export default {
-  props:["index","question"],
+  props: ["index", "question", "open"],
   data() {
     return {
-      Qindex: 1,
-      Qname: "张颜",
-      question: "你知道了什么,我什么都不知道",
-      selects: [
-        { content: "hello", isRight: false },
-        { content: "hello", isRight: true },
-        { content: "hello", isRight: false }
-      ]
+      // Qindex: 1,
+      Qname: "张颜"
+      // question: "你知道了什么,我什么都不知道",
+      // selects: [
+      //   { content: "hello", isRight: false },
+      //   { content: "hello", isRight: true },
+      //   { content: "hello", isRight: false }
+      // ]
     };
   },
   methods: {
@@ -36,10 +36,11 @@ export default {
       this.isRight(e);
     },
     isRight(index) {
-      let arr = this.selects;
+      let arr = this.question.selects;
       let obj = arr[index];
       let answerRight = obj.isRight;
       console.log(answerRight);
+      this.$emit("answer", { isTrue: answerRight });
     }
   }
 };
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <QSmodal></QSmodal>
+  <div>
+    <button @click="begin()">开始</button>
+    <QSmodal :open="qsOpen" :question="nowQs" :index="nowIndex" @answer="getAnswer"></QSmodal>
+  </div>
 </template>
 <script>
 import "./style.css";
@@ -7,6 +10,63 @@ import QSmodal from "../../components/QSmodal/index.vue";
 export default {
   components: {
     QSmodal
+  },
+  data() {
+    return {
+      Qs: [
+        {
+          question: "1111你知道了什么不知道",
+          selects: [
+            { content: "hello", isRight: false },
+            { content: "hello", isRight: true },
+            { content: "hello", isRight: false }
+          ]
+        },
+        {
+          question: "22222你知道么都不知道",
+          selects: [
+            { content: "hello1", isRight: false },
+            { content: "hello", isRight: false },
+            { content: "hello2", isRight: true },
+            { content: "hello", isRight: false }
+          ]
+        },
+        {
+          question: "33333你知么都不知道",
+          selects: [
+            { content: "hello", isRight: false },
+            { content: "hello", isRight: true },
+            { content: "hello", isRight: false },
+            { content: "hello", isRight: false },
+            { content: "hello", isRight: false }
+          ]
+        }
+      ],
+      index: 0,
+      nowIndex: 0,
+      nowQs: [],
+      qsOpen: false,
+      resOpen: false,
+      result: null
+    };
+  },
+  methods: {
+    begin() {
+      this.result = null;
+      this.qsOpen = true;
+      this.nowIndex = this.index;
+      let obj = this.Qs[this.index];
+      this.nowQs = obj;
+      console.log(obj);
+      let idx = this.index + 1;
+      this.index = idx;
+    },
+    getAnswer(e) {
+      console.log(e);
+      let answer = e.isTure;
+      this.result = answer;
+      this.qsOpen = false;
+    }
   }
 };
 </script>
